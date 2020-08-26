a166d0f9f0d368e7649b7b592c58ecf158b3eabd
Alvin Coulson <aaacoulson@gmail.com>
2020-08-26 12:48:18 -0400

    add schema.rb and seed db

--- chronos-backend/db/seeds.rb
+++ chronos-backend/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Employee.destroy_all
+
+c = 3
+c.times do
+    Employee.create(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name)
+end
+puts "successfully created" if Employee.count == 3
+
+Employee.all.each do |emp|
+    emp.update(email: emp.first_name[0].downcase + emp.last_name.downcase + "@" + "zmart.com")
+end
+puts "#{3} emails successfully added" if Employee.count {|e| e.first_name} == 3
