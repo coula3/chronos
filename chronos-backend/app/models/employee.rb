4da751b1772157c1fb4690a4bcd4b442552844be
Alvin Coulson <aaacoulson@gmail.com>
2020-08-26 15:00:58 -0400

    add validations to models

--- chronos-backend/app/models/employee.rb
+++ chronos-backend/app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ApplicationRecord
     has_many :time_events
+    
+    validates :first_name, :last_name, :email, presence: true
+    validates :email, uniqueness: { case_sensitive: false }
 end
--- chronos-backend/app/models/time_event.rb
+++ chronos-backend/app/models/time_event.rb
@@ -1,3 +1,5 @@
 class TimeEvent < ApplicationRecord
   belongs_to :employee
+
+  validates :date, presence: true
 end
