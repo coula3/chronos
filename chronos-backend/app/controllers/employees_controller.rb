b2396232adf14158c1a03edd230eac0e82aed206
Alvin Coulson <aaacoulson@gmail.com>
2020-08-26 19:45:46 -0400

    add signin action to employees controller

--- chronos-backend/app/controllers/employees_controller.rb
+++ chronos-backend/app/controllers/employees_controller.rb
@@ -1,4 +1,10 @@
 class EmployeesController < ApplicationController
+
+    def signin
+        employee = Employee.find_by(email: params[:email])
+        render json: employee
+    end
+
     def index
         employees = Employee.all
         render json: employees
