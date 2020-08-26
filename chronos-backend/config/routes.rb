d6bfc6f2de53078d493675da94098bbfa2c37112
Alvin Coulson <aaacoulson@gmail.com>
2020-08-26 19:44:14 -0400

    add a custom route for signin by email

--- chronos-backend/config/routes.rb
+++ chronos-backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/employees/:email', to: "employees#signin", constraints: { email: /[^\/]+/}
+  
   resources :time_events
   resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
