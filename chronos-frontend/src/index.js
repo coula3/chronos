0db7d10456681903e9f1547b992cc6749d3b2a02
Alvin Coulson <aaacoulson@gmail.com>
2020-08-26 21:30:50 -0400

    add styling and attributes to h2 that renders employee name

--- chronos-frontend/src/index.js
+++ chronos-frontend/src/index.js
@@ -20,5 +20,12 @@ function getEmployeeData() {
 function renderEmployeeData(employeeObject) {
     const h2 = document.createElement("h2");
     h2.innerText = `Hi ${employeeObject.first_name}!`
-    document.getElementById("div-signup-signin").after(h2)
+    h2.setAttribute("id", "employee-name")
+    h2.setAttribute("employee-data-id", employeeObject.id)
+    document.getElementById("main-container").appendChild(h2)
+    h2.style.clear = "both";
+    h2.style.paddingTop = "25px"
+    // document.querySelector("#employee-name").getAttribute("employee-data-id")
+
+    renderEmployeeTimeEvents(employeeObject)
 }
