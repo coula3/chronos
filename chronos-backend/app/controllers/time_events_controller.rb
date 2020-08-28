b51f9d8218246f3ec9cc5616509d992f143abc30
Alvin Coulson <aaacoulson@gmail.com>
2020-08-27 23:15:20 -0400

    add create action on time_events_controller

--- chronos-backend/app/controllers/time_events_controller.rb
+++ chronos-backend/app/controllers/time_events_controller.rb
@@ -3,4 +3,14 @@ class TimeEventsController < ApplicationController
         time_events = TimeEvent.all
         render json: time_events
     end
+
+    def create
+        employee = Employee.find_by(id: params[:employee_id])
+        time_event = employee.time_events.build(date: params[:date], time_in: params[:time_in])
+        if time_event.save
+            render json: time_event
+        else
+            render json: {message: "Clock In Failure"}
+        end
+    end
 end
