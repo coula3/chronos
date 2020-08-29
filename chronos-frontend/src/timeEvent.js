a7a18c9d09b05fc59e4ca6fbc91660eba8185999
Alvin Coulson <aaacoulson@gmail.com>
2020-08-28 20:07:07 -0400

    add shift data to rendered new time event

--- chronos-frontend/src/timeEvent.js
+++ chronos-frontend/src/timeEvent.js
@@ -165,7 +165,8 @@ function renderNewTimeEvent(event) {
         <span id="span-event-time-in" style="margin: 0px 25px 0px 25px">${event.time_in ? getTime(event.time_in) : ""}</span>
         <span id="span-event-time-out" style="margin: 0px 25px 0px 25px">${event.time_out ? getTime(event.time_out) : ""}</span>
         <span id="span-event-break-start" style="margin: 0px 25px 0px 25px">${event.break_start ? getTime(event.break_start) : ""}</span>
-        <span id="span-event-break-end" style="clear:right; margin: 0px 25px 0px 25px">${event.break_end ? getTime(event.break_end) : ""}</span>
+        <span id="span-event-break-end" style="margin: 0px 25px 0px 25px">${event.break_end ? getTime(event.break_end) : ""}</span>
+        <span id="span-event-shift" style="clear:right; margin: 0px 25px 0px 25px">${getShift(getTime(event.time_in))}</span>
         
         <button id="btn-break-resume" style="float:right; width:100px;" disabled>Take Break</button>`;
     
