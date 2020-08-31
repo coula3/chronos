0d27c8b4644d096a1fe2c8758e778e0ccfdf1c88
Alvin Coulson <aaacoulson@gmail.com>
2020-08-31 09:47:25 -0400

    add the position attribute for serialization

--- chronos-backend/app/serializers/employee_serializer.rb
+++ chronos-backend/app/serializers/employee_serializer.rb
@@ -1,4 +1,4 @@
 class EmployeeSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email
+  attributes :id, :first_name, :last_name, :email, :position
   has_many :time_events
 end
