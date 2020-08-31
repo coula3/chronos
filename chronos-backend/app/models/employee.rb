bdd366df8796003257e0aed5c7782f3a68cf6599
Alvin Coulson <aaacoulson@gmail.com>
2020-08-31 16:32:22 -0400

    add position attribute to presence validation on Employee model

--- chronos-backend/app/models/employee.rb
+++ chronos-backend/app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
     has_many :time_events
     
-    validates :first_name, :last_name, :email, presence: true
+    validates :first_name, :last_name, :position, :email, presence: true
     validates :email, uniqueness: { case_sensitive: false }
     validates :email, email: true
 end
