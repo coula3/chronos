53dae042b559ed9f45c9dfcbdecd22f03d143b1a
Alvin Coulson <aaacoulson@gmail.com>
2020-08-31 09:45:34 -0400

    add the position attribute to strong params

--- chronos-backend/app/controllers/employees_controller.rb
+++ chronos-backend/app/controllers/employees_controller.rb
@@ -25,6 +25,6 @@ class EmployeesController < ApplicationController
 
     private
     def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :email)
+        params.require(:employee).permit(:first_name, :last_name, :email, :position)
     end
 end
