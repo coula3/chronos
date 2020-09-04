bd2cec10800d34f3ecf27d6ec4ffa000479c6abb
Alvin Coulson <aaacoulson@gmail.com>
2020-09-04 13:58:33 -0400

    revise update action on time_events controller to process clock out while user is on break

--- chronos-backend/app/controllers/time_events_controller.rb
+++ chronos-backend/app/controllers/time_events_controller.rb
@@ -18,19 +18,25 @@ class TimeEventsController < ApplicationController
         time_event = TimeEvent.find_by(id: params[:id])
         message = {message: time_event.errors.full_messages}
 
-        if params.keys.include?("break_start")
+        if params[:break_start]
             if time_event.update(break_start: time_event_params[:break_start])
                 render json: time_event
             else
                 render json: message
             end
-        elsif params.keys.include?("break_end")
+        elsif params[:break_end] && params[:time_out]
+            if time_event.update(break_end: time_event_params[:break_end], time_out: time_event_params[:time_out])
+                render json: time_event
+            else
+                render json: message
+            end
+        elsif params[:break_end] && !params[:time_out]
             if time_event.update(break_end: time_event_params[:break_end])
                 render json: time_event
             else
                 render json: message
             end
-        elsif params.keys.include?("time_out")
+        elsif params[:time_out]
             if time_event.update(time_out: time_event_params[:time_out])
                 render json: time_event
             else
