ffde0d4a2e771c343f3ada12ddb63f4d71631414
Alvin Coulson <aaacoulson@gmail.com>
2020-09-09 17:01:18 -0400

    replace .floor() with .round() on getRunningTime()

--- chronos-frontend/src/sharedResources.js
+++ chronos-frontend/src/sharedResources.js
@@ -107,8 +107,8 @@ function getHours(startTime, finishTime) {
 }
 
 function getRunningTime(milleseconds){
-    const hours = (Math.floor(milleseconds / 3600) % 24) < 10 ? `0${Math.floor(milleseconds / 3600) % 24}` : Math.floor(milleseconds / 3600) % 24
-    const minutes = (Math.floor(milleseconds / 60) % 60) < 10 ? `0${Math.floor(milleseconds / 60) % 60}` : Math.floor(milleseconds / 60) % 60
+    const hours = (Math.round(milleseconds / 3600) % 24) < 10 ? `0${Math.round(milleseconds / 3600) % 24}` : Math.round(milleseconds / 3600) % 24
+    const minutes = (Math.round(milleseconds / 60) % 60) < 10 ? `0${Math.round(milleseconds / 60) % 60}` : Math.round(milleseconds / 60) % 60
     const secs =  milleseconds % 60 < 10 ? `0${milleseconds % 60 }` : milleseconds % 60
     return `${hours}:${minutes}:${secs}`
 }
