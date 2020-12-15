a4e5460abdda6ce0e2dc6956e9761b082c89f0a1
Alvin Coulson <aaacoulson@gmail.com>
2020-12-15 16:42:10 -0500

    fire 'reSignInEmployee' from index.js

--- chronos-frontend/src/index.js
+++ chronos-frontend/src/index.js
@@ -1,1 +1,7 @@
 const CHRONOS_URL = "http://localhost:3000/api/v1";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const isAuthenticated = localStorage.getItem('jwt_token');
+    const data = JSON.parse(localStorage.getItem('data'));
+    isAuthenticated && reSignInEmployee(data);
+})
