877375899cb0a506ad987e20208dc8ceedf0db61
Alvin Coulson <aaacoulson@gmail.com>
2020-12-15 15:11:15 -0500

    fix bug on the render of serialized object from AuthController

--- chronos-backend/app/controllers/api/v1/auth_controller.rb
+++ chronos-backend/app/controllers/api/v1/auth_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AuthController < ApplicationController
         token = encode_token(user_id: @employee.id)
 
         if @employee && @employee.authenticate(employee_params[:password])
-            render json: { employee: @employee, jwt: token }, status: :accepted
+            render json: { employee: EmployeeSerializer.new(@employee), jwt: token }, status: :accepted
         else
             render json: {message: "No user account matches email"}, status: :not_acceptable
         end
