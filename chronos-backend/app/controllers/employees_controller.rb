0614101d77439d0f798d40e382d5cf18a128be5d
Alvin Coulson <aaacoulson@gmail.com>
2020-12-14 21:49:59 -0500

    update 'employee_params' with password attribute on EmployeesController

--- chronos-backend/app/controllers/employees_controller.rb
+++ chronos-backend/app/controllers/employees_controller.rb
@@ -7,12 +7,11 @@ class EmployeesController < ApplicationController
 
     def create
         employee = Employee.new(employee_params)
-        
         employee.save ? (render json: employee) : (render json: {message: employee.errors.full_messages.join("; ")})
     end
 
     private
     def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :email, :position)
+        params.require(:employee).permit(:first_name, :last_name, :email, :position, :password)
     end
 end
