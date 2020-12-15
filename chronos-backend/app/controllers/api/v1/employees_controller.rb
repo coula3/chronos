08531acab91d9dc698e6d540321f11b21a7aba1d
Alvin Coulson <aaacoulson@gmail.com>
2020-12-15 17:43:39 -0500

    fix bug on undefined local variable

--- chronos-backend/app/controllers/api/v1/employees_controller.rb
+++ chronos-backend/app/controllers/api/v1/employees_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::EmployeesController < ApplicationController
             token = encode_token(user_id: @employee.id)
             render json: { employee: @employee, jwt: token }, status: :created
         else
-            render json: {message: employee.errors.full_messages.join("; ")}, status: :not_acceptable
+            render json: {message: @employee.errors.full_messages.join("; ")}, status: :not_acceptable
         end
     end
 
