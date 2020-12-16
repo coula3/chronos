ab91e8b2374d8c4ba65604b59c7119e5fa529aef
Alvin Coulson <aaacoulson@gmail.com>
2020-12-15 22:55:55 -0500

    replace params with strong params on actions on TimeEventsController

--- chronos-backend/app/controllers/api/v1/time_events_controller.rb
+++ chronos-backend/app/controllers/api/v1/time_events_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TimeEventsController < ApplicationController
     def create
-        employee = Employee.find_by(id: params[:employee_id])
+        employee = Employee.find_by(id: time_event_params[:employee_id])
         time_event = employee.time_events.build(date: time_event_params[:date])
 
         time_event.save ? (render json: time_event) : (render json: {message: "Clock In unsuccessful"})
@@ -10,19 +10,19 @@ class Api::V1::TimeEventsController < ApplicationController
         time_event = TimeEvent.find_by(id: time_event_params[:id])
         message = {message: time_event.errors.full_messages}
 
-        if params[:break_start]
+        if time_event_params[:break_start]
             time_event.update(break_start: time_event_params[:break_start]) ? (render json: time_event) : (render json: message)
-        elsif params[:break_end] && params[:time_out]
+        elsif time_event_params[:break_end] && time_event_params[:time_out]
             time_event.update(break_end: time_event_params[:break_end], time_out: time_event_params[:time_out]) ? (render json: time_event) : (render json: message)
-        elsif params[:break_end] && !params[:time_out]
+        elsif time_event_params[:break_end] && !time_event_params[:time_out]
             time_event.update(break_end: time_event_params[:break_end]) ? (render json: time_event) : (render json: message)
-        elsif params[:time_out]
+        elsif time_event_params[:time_out]
             time_event.update(time_out: time_event_params[:time_out]) ? (render json: time_event) : (render json: message)
         end
     end
 
     private
     def time_event_params
-        params.require(:time_event).permit(:id, :date, :break_start, :break_end, :time_out)
+        params.require(:time_event).permit(:id, :date, :break_start, :break_end, :time_out, :employee_id)
     end
 end
