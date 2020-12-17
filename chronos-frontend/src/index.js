d3a1e66348efd5a4b2455bd7d9c2dc44689334da
Alvin Coulson <aaacoulson@gmail.com>
2020-12-16 19:45:22 -0500

    declare 'getTimeEventStatus'

--- chronos-frontend/src/index.js
+++ chronos-frontend/src/index.js
@@ -1,17 +1,37 @@
 const CHRONOS_URL = "http://localhost:3000/api/v1";
 
+let breakStarted, breakEnded, clockedOut;
+
 document.addEventListener("DOMContentLoaded", () => {
     const localStorageData = JSON.parse(localStorage.getItem('data'));
+    const currentTimeEvent = JSON.parse(localStorage.getItem('newTimeEvent'));
     const employeeId = localStorageData && localStorageData.employee.id;
     const isAuthenticated = localStorage.getItem('jwt_token');
     const editMode = JSON.parse(localStorage.getItem('editMode'));
 
-    if(isAuthenticated && editMode){
+    getTimeEventStatus(currentTimeEvent);
+
+    if(breakStarted && breakEnded && clockedOut){
+        updateDOM(employeeId);
+    } else if(isAuthenticated && editMode){
         updateDOM(employeeId);
     } else if(isAuthenticated){
         reSignInEmployee(localStorageData);
     }
 })
+
+function getTimeEventStatus(currentTimeEvent){
+    if(currentTimeEvent && currentTimeEvent.timeOut){
+        breakStarted = true;
+        breakEnded = true;
+        clockedOut = true;
+    } else if(currentTimeEvent && currentTimeEvent.breakEnd){
+        breakStarted = true;
+        breakEnded = true;
+    } else if(currentTimeEvent && currentTimeEvent.breakStart){
+        breakStarted = true;
+    }
+}
 
 function updateDOM(employeeId){
     fetch(`${CHRONOS_URL}/employees/${employeeId}`, {
