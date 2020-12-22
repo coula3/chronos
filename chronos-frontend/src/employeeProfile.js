558c2def70a6a90d7f8a024cc7d3d0db84ab15e4
Alvin Coulson <aaacoulson@gmail.com>
2020-12-22 09:50:21 -0500

    use conditional to switch between employee profile and time data

--- chronos-frontend/src/employeeProfile.js
+++ chronos-frontend/src/employeeProfile.js
@@ -3,28 +3,35 @@ divEmployeeProfile.setAttribute("id", "div-emp-profile");
 divEmployeeProfile.style.cssText = "clear: both";
 
 buttonProfile.addEventListener("click", (e) => {
-    clearInterval(runningTimeInterval);
-    localStorage.setItem("isRendered", "employeeProfile");
+    if(e.target.innerText === "Profile"){
+        clearInterval(runningTimeInterval);
+        localStorage.setItem("rendered", "Profile");
 
-    document.getElementById("events-container") && document.getElementById("events-container").remove();
-    e.target.innerText = "Time Data";
-    document.getElementById("span-message").innerText = "";
-    document.getElementById("span-message").after(divEmployeeProfile);
+        document.getElementById("events-container") && document.getElementById("events-container").remove();
+        e.target.innerText = "Time Data";
+        document.getElementById("span-message").innerText = "";
+        document.getElementById("span-message").after(divEmployeeProfile);
 
-    divEmployeeProfile.innerHTML += `
-        <div id="profile-card" style="padding-top: 15px">
-            <p class="profile-p profile-caption">Name</p>
-            <p class="profile-p">Ken Fry</p>
+        divEmployeeProfile.innerHTML += `
+            <div id="profile-card" style="padding-top: 15px;">
+                <p class="profile-p profile-caption">Name</p>
+                <p class="profile-p">Ken Fry</p>
 
-            <p class="profile-p profile-caption">Position</p>
-            <p class="profile-p">Customer Associate</p>
+                <p class="profile-p profile-caption">Position</p>
+                <p class="profile-p">Customer Associate</p>
 
-            <p class="profile-p profile-caption">eMail</p>
-            <p class="profile-p">k@fry.com</p>
-            <div style="padding: 15px; background-color: #e6ccb3;">
-                <button style="padding: 3px 5px; width: 200px; font-size: 17px">Edit</button>
-                <button style="padding: 3px 5px; width: 200px; font-size: 17px">Change Password</button>
+                <p class="profile-p profile-caption">eMail</p>
+                <p class="profile-p">k@fry.com</p>
+                <div style="padding: 15px; background-color: #e6ccb3;">
+                    <button style="padding: 3px 5px; width: 200px; font-size: 17px">Edit</button>
+                    <button style="padding: 3px 5px; width: 200px; font-size: 17px">Change Password</button>
+                </div>
             </div>
-        </div>
-    `;
+        `;
+
+    } else {
+        divEmployeeProfile.remove();
+        e.target.innerText = "Profile";
+        location.reload();
+    }
 })
