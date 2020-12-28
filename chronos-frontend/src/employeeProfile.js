7f7796266efa673428db4554a2ab079bf6800773
Alvin Coulson <aaacoulson@gmail.com>
2020-12-27 19:39:00 -0500

    abstract out 'appendEmployeeProfile' from event listener on employeeProfile.js

--- chronos-frontend/src/employeeProfile.js
+++ chronos-frontend/src/employeeProfile.js
@@ -4,35 +4,12 @@ divEmployeeProfile.style.cssText = "clear: both";
 
 buttonProfile.addEventListener("click", (e) => {
     const localStorageData = JSON.parse(localStorage.getItem('data'));
+
     if(e.target.innerText === "Profile"){
         clearInterval(runningTimeInterval);
         localStorage.setItem("rendered", "Profile");
-        buttonClockInOut.disabled = true;
-        buttonClockInOut.style.cssText = "background-color: null, color: #000"
 
-        document.getElementById("p-new-user-msg") && document.getElementById("p-new-user-msg").remove();
-        document.getElementById("events-container") && document.getElementById("events-container").remove();
-        e.target.innerText = "Time Data";
-        document.getElementById("span-message").innerText = "";
-        document.getElementById("span-message").after(divEmployeeProfile);
-
-        divEmployeeProfile.innerHTML += `
-            <div id="profile-card">
-                <h2 id="profileHeader">Profile</h2>
-                <p class="profileCaption">Name</p>
-                <p class="profileParagraph">${localStorageData.employee.first_name} ${localStorageData.employee.last_name}</p>
-
-                <p class="profileCaption">Position</p>
-                <p class="profileParagraph">${localStorageData.employee.position}</p>
-
-                <p class="profileCaption">Email</p>
-                <p class="profileParagraph">${localStorageData.employee.email}</p>
-                <div id="profileButtonsDiv">
-                    <button class="profileButtons">Edit</button>
-                    <button class="profileButtons">Change Password</button>
-                </div>
-            </div>
-        `;
+        appendEmployeeProfile(localStorageData)
 
     } else {
         divEmployeeProfile.remove();
@@ -42,4 +19,30 @@ buttonProfile.addEventListener("click", (e) => {
     }
 })
 
-// document.addEventListener("DOMContentLoaded", () => console.log("Hello world!"))
+function appendEmployeeProfile(localStorageData){
+    buttonClockInOut.disabled = true;
+    buttonClockInOut.style.cssText = "background-color: null, color: #000";
+    document.getElementById("p-new-user-msg") && document.getElementById("p-new-user-msg").remove();
+    document.getElementById("events-container") && document.getElementById("events-container").remove();
+    document.getElementById("btn-profile").innerText = "Time Data";
+    document.getElementById("span-message").innerText = "";
+    document.getElementById("span-message").after(divEmployeeProfile);
+
+    divEmployeeProfile.innerHTML += `
+        <div id="profile-card">
+            <h2 id="profileHeader">Profile</h2>
+            <p class="profileCaption">Name</p>
+            <p class="profileParagraph">${localStorageData.employee.first_name} ${localStorageData.employee.last_name}</p>
+
+            <p class="profileCaption">Position</p>
+            <p class="profileParagraph">${localStorageData.employee.position}</p>
+
+            <p class="profileCaption">Email</p>
+            <p class="profileParagraph">${localStorageData.employee.email}</p>
+            <div id="profileButtonsDiv">
+                <button class="profileButtons">Edit</button>
+                <button class="profileButtons">Change Password</button>
+            </div>
+        </div>
+    `;
+}
