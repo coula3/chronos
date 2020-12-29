a6a98101a18206ddd96b0be058b469fb420f6a58
Alvin Coulson <aaacoulson@gmail.com>
2020-12-28 21:11:54 -0500

    create editProfile.js

---
+++ chronos-frontend/src/scripts/editProfile.js
@@ -0,0 +1,14 @@
+const editProfileCard = `
+    <div id="editProfileCard">
+    <h2 class="profileHeader">Edit Profile</h2>
+    <form id="editProfileForm">
+        <p class="profileCaption">First name</p>
+        <p class="profileParagraph"><input id="profileFirstName" class="profileInputs" type="text"></input></p>
+
+        <div class="profileButtonsDiv">
+            <button id="profileSaveBtn" class="profileButtons">Save</button>
+            <button id="editProfileToProfileBtn" class="profileButtons">Profile</button>
+        </div>
+    </form>
+    </div>
+`;
