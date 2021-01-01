e43eb6a7c508bab3fd4a4f036cb2b8fed44ecf78
Alvin Coulson <aaacoulson@gmail.com>
2021-01-01 12:41:43 -0500

    create sign in form dynamically at app launch

---
+++ chronos-frontend/src/scripts/signInForm.js
@@ -0,0 +1,18 @@
+const signInForm = `
+    <div id="div-signup-signin">
+    <div id="div-signin">
+        <form id="form-signin">
+            <div id="div-signin-inputs">
+                <input id="sign-in-email" class="input" type="text" name="sign-in-email" placeholder="email" />
+                <input id="sign-in-password" class="input" type="password" name="sign-in-password" placeholder="password" />
+            </div>
+            <button id="btn-sign-in" class="button">Sign In</button>
+        </form>
+    </div>
+    <div id="div-signup">
+        <form id="form-create-employee">
+            <button id="btn-create-user" class="button">Sign Up</button>
+        </form>
+    </div>
+    </div>
+`;
