c0b377f58d0c0d60fc7bd773922e74a6210fad4b
Alvin Coulson <aaacoulson@gmail.com>
2021-01-01 11:25:48 -0500

    add app secret to JWT encode and decode methods on ApplicationController

--- chronos-backend/app/controllers/application_controller.rb
+++ chronos-backend/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
     before_action :authorized
 
     def encode_token(payload)
-        JWT.encode(payload, '#101010#')
+        JWT.encode(payload, Figaro.env.chronos_secret)
     end
 
     def auth_header
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
         if auth_header
             token = auth_header.split(' ')[1]
             begin
-                JWT.decode(token, '#101010#', true, algorithm: 'HS256')
+                JWT.decode(token, Figaro.env.chronos_secret, true, algorithm: 'HS256')
             rescue JWT::DecodeError
                 nil
             end
