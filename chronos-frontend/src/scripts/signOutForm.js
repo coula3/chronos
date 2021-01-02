75657597d53656a226f46390b642089d78bfa7ed
Alvin Coulson <aaacoulson@gmail.com>
2021-01-02 08:30:30 -0500

    abstract out sign out form to a separate file

---
+++ chronos-frontend/src/scripts/signOutForm.js
@@ -0,0 +1,26 @@
+const employeePositions = ["Customer Associate", "Fresh Food Associate", "Manager", "Sales Associate", "Stocker"];
+
+function appendEmployeeFormElements(){
+    const defaultValue = "--choose your position--";
+    const positionList = [...employeePositions];
+    positionList.unshift(defaultValue);
+
+    const positionOptions = positionList.map(position => {
+        const optionValue = position !== "--choose your position--" ? position : "";
+        return  `<option value=${optionValue}>${position}</option>`;
+    });
+
+    const formElements = `
+        <div id="div-input-elements" style="display:inline-block">
+            <input id="first-name" class="input" type="text" name="first-name" placeholder="first name" />
+            <input id="last-name" class="input" type="text" name="last-name" placeholder="last name" />
+            <select type="select" id="position" name="position">
+               ${positionOptions}
+            </select>
+            <input id="email" class="input" type="text" name="email" placeholder="email" />
+            <input id="password" class="input" type="password" name="password" placeholder="password" />
+            <input id="password-confirmation" class="input" type="password" name="password-confirmation" placeholder="password confirmation" />
+        </div>
+    `
+    buttonCreateUser.insertAdjacentHTML('beforebegin', formElements);
+}
--- chronos-frontend/src/scripts/utility.js
+++ chronos-frontend/src/scripts/utility.js
@@ -1,33 +1,7 @@
 const buttonAdmin = document.createElement("button");
 const buttonProfile = document.createElement("button");
 const spanMsg = document.createElement("span");
-const employeePositions = ["Customer Associate", "Fresh Food Associate", "Manager", "Sales Associate", "Stocker"];
 let messageTimeout;
-
-function appendEmployeeFormElements(){
-    const defaultValue = "--choose your position--";
-    const positionList = [...employeePositions];
-    positionList.unshift(defaultValue);
-
-    const positionOptions = positionList.map(position => {
-        const optionValue = position !== "--choose your position--" ? position : "";
-        return  `<option value=${optionValue}>${position}</option>`;
-    });
-
-    const formElements = `
-        <div id="div-input-elements" style="display:inline-block">
-            <input id="first-name" class="input" type="text" name="first-name" placeholder="first name" />
-            <input id="last-name" class="input" type="text" name="last-name" placeholder="last name" />
-            <select type="select" id="position" name="position">
-               ${positionOptions}
-            </select>
-            <input id="email" class="input" type="text" name="email" placeholder="email" />
-            <input id="password" class="input" type="password" name="password" placeholder="password" />
-            <input id="password-confirmation" class="input" type="password" name="password-confirmation" placeholder="password confirmation" />
-        </div>
-    `
-    buttonCreateUser.insertAdjacentHTML('beforebegin', formElements);
-}
 
 function instantiateEmployeeObject(employeeObject){
     return new Employee(employeeObject.id, employeeObject.first_name, employeeObject.last_name, employeeObject.position, employeeObject.email, employeeObject.time_events);
