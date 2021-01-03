9133b67d082bacc6ca7e4a994f4b1e9f13d1c16e
Alvin Coulson <aaacoulson@gmail.com>
2021-01-02 19:56:35 -0500

    refactor editProfile form

--- chronos-frontend/src/scripts/editProfile.js
+++ chronos-frontend/src/scripts/editProfile.js
@@ -16,20 +16,18 @@ function generateEditProfileCard(){
         <h2 class="profileHeader">Edit Profile</h2>
         <form id="editProfileForm">
             <p class="profileCaption">First name</p>
-            <p class="profileParagraph"><input id="profileFirstName" class="editProfileInputs" type="text" value=${localStorageData.employee.first_name}></input></p>
+            <input id="profileFirstName" class="editProfileInputs" type="text" value=${localStorageData.employee.first_name}></input>
 
             <p class="profileCaption">Last name</p>
-            <p class="profileParagraph"><input id="profileLastName" class="editProfileInputs" type="text" value=${localStorageData.employee.last_name}></input></p>
+            <input id="profileLastName" class="editProfileInputs" type="text" value=${localStorageData.employee.last_name}></input>
 
             <p class="profileCaption">Position</p>
-            <p class="profileParagraph">
                 <select type="select" id="profilePosition" class="editProfileInputs" name="position">
                     ${positionOptions}
                 </select>
-            </p>
 
             <p class="profileCaption">Email</p>
-            <p class="profileParagraph"><input id="profileEmail" class="editProfileInputs" type="text" value=${localStorageData.employee.email}></input></p>
+            <input id="profileEmail" class="editProfileInputs" type="text" value=${localStorageData.employee.email}></input>
 
             <div class="profileButtonsDiv">
                 <button id="profileSaveBtn" class="profileButtons">Save</button>
