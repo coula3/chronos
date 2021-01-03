e21ddc9aab2a950fb044c54deb72ce18c09f22fa
Alvin Coulson <aaacoulson@gmail.com>
2021-01-02 20:18:24 -0500

    create methods to render error messages on editProfile

--- chronos-frontend/src/scripts/editProfileErrMessages.js
+++ chronos-frontend/src/scripts/editProfileErrMessages.js
@@ -15,3 +15,30 @@ function renderEditProfileErrors(messages){
         }
     });
 }
+
+function renderEditProfileFirstNameErr(firstNameMsg, spanStyles){
+    spanFirstName = document.createElement("span");
+    spanFirstName.innerText = firstNameMsg;
+    spanFirstName.setAttribute("id", "spanFirstName");
+    spanFirstName.style.cssText = spanStyles;
+    document.getElementById("profileFirstName").style.cssText = "margin-bottom: 0px";
+    !document.getElementById("spanFirstName") && document.getElementById("profileFirstName").after(spanFirstName);
+}
+
+function renderEditProfileLastNameErr(lastNameMsg, spanStyles){
+    spanLastName = document.createElement("span");
+    spanLastName.innerText = lastNameMsg;
+    spanLastName.setAttribute("id", "spanLastName");
+    spanLastName.style.cssText = spanStyles;
+    document.getElementById("profileLastName").style.cssText = "margin-bottom: 0px";
+    !document.getElementById("spanLastName") && document.getElementById("profileLastName").after(spanLastName);
+}
+
+function renderEditProfileEmailErr(emailMsg, spanStyles){
+    spanEmail = document.createElement("span");
+    spanEmail.innerText = emailMsg;
+    spanEmail.setAttribute("id", "spanEmail");
+    spanEmail.style.cssText = spanStyles;
+    document.getElementById("profileEmail").style.cssText = "margin-bottom: 0px";
+    !document.getElementById("spanEmail") && document.getElementById("profileEmail").after(spanEmail);
+}
