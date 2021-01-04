195bc0e90e652fb5e8477531c54b8738f2688512
Alvin Coulson <aaacoulson@gmail.com>
2021-01-04 14:39:01 -0500

    refactor update action on TimeEventsController to handle close out of time event with nil break value from FE and return employee object as JSON response

--- chronos-backend/app/controllers/api/v1/time_events_controller.rb
+++ chronos-backend/app/controllers/api/v1/time_events_controller.rb
@@ -8,16 +8,21 @@ class Api::V1::TimeEventsController < ApplicationController
 
     def update
         time_event = TimeEvent.find_by(id: time_event_params[:id])
+        @employee = TimeEvent.find_by(id: time_event_params[:id]).employee
         message = {message: time_event.errors.full_messages}
 
         if time_event_params[:break_start]
             time_event.update(break_start: time_event_params[:break_start]) ? (render json: time_event) : (render json: message)
-        elsif time_event_params[:break_end] && time_event_params[:time_out]
-            time_event.update(break_end: time_event_params[:break_end], time_out: time_event_params[:time_out]) ? (render json: time_event) : (render json: message)
         elsif time_event_params[:break_end] && !time_event_params[:time_out]
             time_event.update(break_end: time_event_params[:break_end]) ? (render json: time_event) : (render json: message)
+        elsif !time_event_params[:break_end] && time_event_params[:time_out]
+            if time_event.update(break_end: time_event_params[:time_out], time_out: time_event_params[:time_out])
+                render json: { employee: EmployeeSerializer.new(@employee) }, status: :ok
+            else
+                render json: message
+            end
         elsif time_event_params[:time_out]
-            close_new_time_event(time_event, message)
+            close_new_time_event(time_event, message, @employee)
         end
     end
 
@@ -35,10 +40,9 @@ class Api::V1::TimeEventsController < ApplicationController
         params.require(:time_event).permit(:id, :date, :break_start, :break_end, :time_out, :employee_id)
     end
 
-    def close_new_time_event(time_event, message)
+    def close_new_time_event(time_event, message, employee)
         if time_event.update(time_out: time_event_params[:time_out])
-            @employee = TimeEvent.find(time_event_params[:id]).employee
-            render json: { employee: EmployeeSerializer.new(@employee) }, status: :ok
+            render json: { employee: EmployeeSerializer.new(employee) }, status: :ok
         else
             render json: message
         end
