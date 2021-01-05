2c82e9e4f3ec24a3831a581e476b89635fe389d7
Alvin Coulson <aaacoulson@gmail.com>
2021-01-05 15:48:21 -0500

    add max length validation of first and last names on Employee

--- chronos-backend/app/models/employee.rb
+++ chronos-backend/app/models/employee.rb
@@ -2,6 +2,8 @@ class Employee < ApplicationRecord
     has_many :time_events
     
     validates :first_name, :last_name, :position, :email, presence: true
+    validates :first_name, :length => { :maximum => 14 }, :if => lambda{ first_name.present? }
+    validates :last_name, :length => { :maximum => 20 }, :if => lambda{ last_name.present? }
     validates :email, uniqueness: { case_sensitive: false }
     validates :email, email: true
     has_secure_password
