6fcb35001e2c17df0fe75698215a2477eb5e86dd
Alvin Coulson <aaacoulson@gmail.com>
2021-01-05 16:05:04 -0500

    add validation to first and last names to accept only letters and hyphen

--- chronos-backend/app/models/employee.rb
+++ chronos-backend/app/models/employee.rb
@@ -3,6 +3,7 @@ class Employee < ApplicationRecord
     
     validates :first_name, :last_name, :position, :email, presence: true
     validates :first_name, :length => { :maximum => 14 }, :if => lambda{ first_name.present? }
+    validates :first_name, :last_name, format: { with: /\A[a-zA-Z\-]+\z/, message: "only allows letters and hyphen" }, :if => lambda{ first_name.present? || last_name.present? }
     validates :last_name, :length => { :maximum => 20 }, :if => lambda{ last_name.present? }
     validates :email, uniqueness: { case_sensitive: false }
     validates :email, email: true
