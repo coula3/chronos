7e42f4d92c14f69d6465eff3c39a26bf2f411492
Alvin Coulson <aaacoulson@gmail.com>
2021-01-19 18:29:13 -0500

    call sort method on copy of array to make it bug proof

--- chronos-frontend/src/scripts/timeEvent.js
+++ chronos-frontend/src/scripts/timeEvent.js
@@ -245,7 +245,7 @@ function renderEmployeeTimeEvents(employeeObject) {
     }
 
     const closedtimeEvents = employeeTimeEvents.filter((e) => { return e.timeOut});
-    const renderedTimeEvents = closedtimeEvents.sort((a, b) => a.id - b.id ).slice(-5);
+    const renderedTimeEvents = [...closedtimeEvents].sort((a, b) => a.id - b.id ).slice(-5);
     const openTimeEvent = employeeTimeEvents.filter((e) => { return !e.timeOut });
 
     const divTimeEvents = document.createElement("div");
